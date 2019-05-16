b8fa7248fe5277ac759326e3bc47966d58ba4c62
nardiyansah <janatri1398@gmail.com>
2019-05-16 09:20:04 +0700

    Add files via upload

---
+++ src/VendingMachineItera/Fungsi.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package VendingMachineItera;
+/**
+ *
+ * @author Nardiyansah
+ */
+public abstract class Fungsi {
+    
+    public abstract boolean isEmpty(int slot[],int x);
+}
